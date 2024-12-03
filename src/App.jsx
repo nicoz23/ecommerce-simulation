0d8721c319e8bd23671652fd8f48c5c21512d7b3
Nicolás Zunino <77420763+nicoz23@users.noreply.github.com>
2024-12-03 04:00:34 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from './pages/Home'
 import Product from './pages/Product'
 import Category from './pages/Category'
